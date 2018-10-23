ccfd0d57d2eabdca30f750af3ca4b8161952cec9
Thekolllol90 <neosvarre@hotmail.se>
2018-10-23 09:28:06 +0200

    Initial commit

---
+++ src/läxa/konstanter.java
@@ -0,0 +1,11 @@
+package läxa;
+
+public class konstanter {
+			public static final double G	= 9.80; // m / s
+			public static final double R	= 8.314; // j mol^-1 k^-1
+			public static final double p_0	= 101325; //pa
+			public static final double c	= 299792458; // m / s
+			public static final double g_swe= 9.82; // m / s
+}
+
+	
