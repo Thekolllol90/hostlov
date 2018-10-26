c9f4c95625cc30619da9fb9db656b42ce2ca2c20
Thekolllol90 <neosvarre@hotmail.se>
2018-10-26 14:31:08 +0200

    gastable
    fyllt i gas tabellen

--- src/läxa/GasTable.java
+++ src/läxa/GasTable.java
@@ -4,8 +4,9 @@ public enum GasTable {
 
 	AIR(1.29, 1.01), 
 	O2(1.43, 0.92), 
-	OXYGEN(1.43, 0.92)
-	;
+	klor(3.21, 0.5),
+	argon(1.78, 0.52),
+	metan(0.72, 2.21);
 
 	double density;
 	double heatCapacity;
